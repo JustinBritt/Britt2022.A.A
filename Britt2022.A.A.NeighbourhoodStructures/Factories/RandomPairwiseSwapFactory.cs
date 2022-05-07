8ab8a487a75e536a146a0361fceb1ca8a77cd4c6
Justin Britt <britt@uwindsor.ca>
2022-05-06 22:11:09 -0400

    Use interface

--- Britt2022.A.A.NeighbourhoodStructures/Factories/RandomPairwiseSwapFactory.cs
+++ Britt2022.A.A.NeighbourhoodStructures/Factories/RandomPairwiseSwapFactory.cs
@@ -10,7 +10,7 @@
         {
         }
 
-        public IRandomPairwiseSwap Create()
+        public INeighbourhoodStructure Create()
         {
             IRandomPairwiseSwap randomPairwiseSwap = null;
 
--- Britt2022.A.A.NeighbourhoodStructures/InterfacesFactories/IRandomPairwiseSwapFactory.cs
+++ Britt2022.A.A.NeighbourhoodStructures/InterfacesFactories/IRandomPairwiseSwapFactory.cs
@@ -4,6 +4,6 @@
 
     public interface IRandomPairwiseSwapFactory : INeighbourhoodStructureFactory
     {
-        IRandomPairwiseSwap Create();
+        INeighbourhoodStructure Create();
     }
 }
