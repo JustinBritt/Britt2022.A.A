c8574a42986d91c84860a52d2851148c46523db4
Justin Britt <britt@uwindsor.ca>
2022-05-06 22:08:23 -0400

    Added interface

--- Britt2022.A.A.NeighbourhoodStructures/Interfaces/IRandomPairwiseSwap.cs
+++ Britt2022.A.A.NeighbourhoodStructures/Interfaces/IRandomPairwiseSwap.cs
@@ -1,25 +1,6 @@
 ﻿namespace Britt2022.A.A.NeighbourhoodStructures.Interfaces
 {
-    using System;
-
-    using Britt2022.A.A.IndexElements.Structs;
-    using Britt2022.A.A.ParameterElements.Structs.SurgicalSpecialties;
-    using Britt2022.A.A.VariableElements.Structs;
-
-    public interface IRandomPairwiseSwap
+    public interface IRandomPairwiseSwap : INeighbourhoodStructure
     {
-        bool SwapMade { get; set; }
-
-        void Swap(
-            ReadOnlySpan<iIndexElement> i,
-            ReadOnlySpan<jIndexElement> j,
-            ReadOnlySpan<kIndexElement> k,
-            ReadOnlySpan<rIndexElement> r,
-            ReadOnlySpan<B1ParameterElement> B1,
-            ReadOnlySpan<F2ParameterElement> F2,
-            Span<xVariableElement> x);
-
-        void UndoSwap(
-            Span<xVariableElement> x);
     }
 }
