e0ee083350ef2cb80985be140f147259c817d457
Justin Britt <britt@uwindsor.ca>
2022-05-06 23:48:24 -0400

    Implement interface

--- Britt2022.A.A.LAHC/Interfaces/ILateAcceptanceHillClimbingBridge.cs
+++ Britt2022.A.A.LAHC/Interfaces/ILateAcceptanceHillClimbingBridge.cs
@@ -1,18 +1,8 @@
 ﻿namespace Britt2022.A.A.LAHC.Interfaces
 {
-    using Britt2022.A.A.ConstructionHeuristic.InterfacesAbstractFactories;
-    using Britt2022.A.A.Models.Interfaces;
-    using Britt2022.A.A.NeighbourhoodStructures.InterfacesAbstractFactories;
     using Britt2022.A.A.StandaloneLocalSearches.Interfaces;
-    using Britt2022.A.A.StandaloneLocalSearches.InterfacesAbstractFactories;
 
-    public interface ILateAcceptanceHillClimbingBridge
+    public interface ILateAcceptanceHillClimbingBridge : IStandaloneLocalSearchBridge
     {
-        void Bridge(
-            IConstructionHeuristicAbstractFactory constructionHeuristicAbstractFactory,
-            INeighbourhoodStructuresAbstractFactory neighbourhoodStructuresAbstractFactory,
-            IStandaloneLocalSearchAbstractFactory standaloneLocalSearchAbstractFactory,
-            IWGPMModel WGPMModel,
-            IStandaloneLocalSearchSolverConfiguration standaloneLocalSearchSolverConfiguration);
     }
 }
--- Britt2022.A.A.SA/Interfaces/ISimulatedAnnealingBridge.cs
+++ Britt2022.A.A.SA/Interfaces/ISimulatedAnnealingBridge.cs
@@ -1,18 +1,8 @@
 ﻿namespace Britt2022.A.A.SA.Interfaces
 {
-    using Britt2022.A.A.ConstructionHeuristic.InterfacesAbstractFactories;
-    using Britt2022.A.A.Models.Interfaces;
-    using Britt2022.A.A.NeighbourhoodStructures.InterfacesAbstractFactories;
     using Britt2022.A.A.StandaloneLocalSearches.Interfaces;
-    using Britt2022.A.A.StandaloneLocalSearches.InterfacesAbstractFactories;
 
-    public interface ISimulatedAnnealingBridge
+    public interface ISimulatedAnnealingBridge : IStandaloneLocalSearchBridge
     {
-        void Bridge(
-            IConstructionHeuristicAbstractFactory constructionHeuristicAbstractFactory,
-            INeighbourhoodStructuresAbstractFactory neighbourhoodStructuresAbstractFactory,
-            IStandaloneLocalSearchAbstractFactory standaloneLocalSearchAbstractFactory,
-            IWGPMModel WGPMModel,
-            IStandaloneLocalSearchSolverConfiguration standaloneLocalSearchSolverConfiguration);
     }
 }
