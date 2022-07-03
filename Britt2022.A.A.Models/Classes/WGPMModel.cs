d40b499c07d6bd51f68421e65d54be081802adda
Justin Britt <britt@uwindsor.ca>
2022-07-03 04:43:46 -0400

    Changes to usings

--- Britt2022.A.A.Models/Classes/WGPMModel.cs
+++ Britt2022.A.A.Models/Classes/WGPMModel.cs
@@ -10,6 +10,8 @@
     using Britt2022.A.A.Constraints.Interfaces;
     using Britt2022.A.A.Constraints.InterfacesAbstractFactories;
     using Britt2022.A.A.Contexts.Interfaces;
+    using Britt2022.A.A.CrossJoinElements.InterfacesAbstractFactories;
+    using Britt2022.A.A.CrossJoinElements.InterfacesFactories;
     using Britt2022.A.A.CrossJoinElements.Structs;
     using Britt2022.A.A.IndexElements.InterfacesAbstractFactories;
     using Britt2022.A.A.IndexElements.InterfacesFactories;
