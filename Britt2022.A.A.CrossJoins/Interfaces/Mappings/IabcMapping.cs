36de9c3f52b536377ad92ca612742516daff664e
Justin Britt <britt@uwindsor.ca>
2022-07-03 19:37:47 -0400

    Changes to usings

--- Britt2022.A.A.CrossJoins/Interfaces/Mappings/IabcMapping.cs
+++ Britt2022.A.A.CrossJoins/Interfaces/Mappings/IabcMapping.cs
@@ -1,9 +1,5 @@
 ﻿namespace Britt2022.A.A.CrossJoins.Interfaces.Mappings
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
-
     public interface IabcMapping
     {
     }
