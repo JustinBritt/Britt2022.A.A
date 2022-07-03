d339e085d9e043a463d34299cfb7ae6b93cd7b89
Justin Britt <britt@uwindsor.ca>
2022-07-03 19:37:23 -0400

    Added methods; Changes to usings

--- Britt2022.A.A.CrossJoins/Interfaces/Mappings/IabMapping.cs
+++ Britt2022.A.A.CrossJoins/Interfaces/Mappings/IabMapping.cs
@@ -1,10 +1,45 @@
 ﻿namespace Britt2022.A.A.CrossJoins.Interfaces.Mappings
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
-
     public interface IabMapping
     {
+        int aDifference(
+            int a,
+            int aLowerBound);
+
+        int baUpperBoundProduct(
+            int aUpperBound,
+            int b);
+
+        int bDifference(
+            int b,
+            int bLowerBound);
+
+        int bDifferenceaUpperBoundProduct(
+            int aUpperBound,
+            int b,
+            int bLowerBound);
+
+        int abOI(
+            int a,
+            int aUpperBound,
+            int b);
+
+        int abZI(
+            int a,
+            int aLowerBound,
+            int aUpperBound,
+            int b,
+            int bLowerBound);
+
+        int SpanIndex(
+            int a,
+            int aUpperBound,
+            int b,
+            int bLowerBound);
+
+        int SpanLength(
+            int aUpperBound,
+            int bLowerBound,
+            int bUpperBound);
     }
 }
