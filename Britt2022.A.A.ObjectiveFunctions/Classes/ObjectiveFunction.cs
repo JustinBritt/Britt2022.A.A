7bf79b4bdce2641a620ea28b8a6f0e436e9cd78d
Justin Britt <britt@uwindsor.ca>
2022-07-03 22:30:30 -0400

    Changes to usings

--- Britt2022.A.A.ObjectiveFunctions/Classes/ObjectiveFunction.cs
+++ Britt2022.A.A.ObjectiveFunctions/Classes/ObjectiveFunction.cs
@@ -9,6 +9,7 @@
     using Britt2022.A.A.ParameterElements.Structs.ScenarioProbabilities;
     using Britt2022.A.A.ParameterElements.Structs.StrategicTargets;
     using Britt2022.A.A.ParameterElements.Structs.Surgeries;
+    using Britt2022.A.A.VariableElements.InterfacesFactories;
     using Britt2022.A.A.VariableElements.Structs;
     using Britt2022.A.A.Variables.Interfaces;
 
