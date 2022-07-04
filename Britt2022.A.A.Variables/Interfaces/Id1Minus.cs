4020d2672c42712bfac391f0e670f1669310de74
Justin Britt <britt@uwindsor.ca>
2022-07-03 20:24:19 -0400

    Changes to usings

--- Britt2022.A.A.Variables/Interfaces/Id1Minus.cs
+++ Britt2022.A.A.Variables/Interfaces/Id1Minus.cs
@@ -8,6 +8,7 @@
     using Britt2022.A.A.IndexElements.Structs;
     using Britt2022.A.A.ParameterElements.Structs.Surgeries;
     using Britt2022.A.A.ParameterElements.Structs.StrategicTargets;
+    using Britt2022.A.A.ResultElements.InterfacesFactories.SurgeonScenarioDeviations;
     using Britt2022.A.A.ResultElements.Structs.SurgeonScenarioDeviations;
     using Britt2022.A.A.VariableElements.Structs;
 
