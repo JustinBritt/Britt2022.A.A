ab40657a49897cb83e676bfaf3f672a7e179ff8a
Justin Britt <britt@uwindsor.ca>
2022-07-03 20:30:03 -0400

    Added method; Changes to usings

--- Britt2022.A.A.ResultElements/AbstractFactories/ResultElementsAbstractFactory.cs
+++ Britt2022.A.A.ResultElements/AbstractFactories/ResultElementsAbstractFactory.cs
@@ -1,11 +1,36 @@
 ﻿namespace Britt2022.A.A.ResultElements.AbstractFactories
 {
+    using Britt2022.A.A.ResultElements.Factories.DayScenarioRecoveryWardCensuses;
+    using Britt2022.A.A.ResultElements.Factories.ScenarioRecoveryWardCensuses;
+    using Britt2022.A.A.ResultElements.Factories.SurgeonOperatingRoomDayAssignments;
+    using Britt2022.A.A.ResultElements.Factories.SurgeonOperatingRoomDayScenarioDeviations;
+    using Britt2022.A.A.ResultElements.Factories.SurgeonScenarioDeviations;
     using Britt2022.A.A.ResultElements.InterfacesAbstractFactories;
+    using Britt2022.A.A.ResultElements.InterfacesFactories.DayScenarioRecoveryWardCensuses;
+    using Britt2022.A.A.ResultElements.InterfacesFactories.ScenarioRecoveryWardCensuses;
+    using Britt2022.A.A.ResultElements.InterfacesFactories.SurgeonOperatingRoomDayAssignments;
+    using Britt2022.A.A.ResultElements.InterfacesFactories.SurgeonOperatingRoomDayScenarioDeviations;
+    using Britt2022.A.A.ResultElements.InterfacesFactories.SurgeonScenarioDeviations;
 
     public sealed class ResultElementsAbstractFactory : IResultElementsAbstractFactory
     {
         public ResultElementsAbstractFactory()
         {
+        }
+
+        public IxResultElementFactory CreatexResultElementFactory()
+        {
+            IxResultElementFactory factory = null;
+
+            try
+            {
+                factory = new xResultElementFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
         }
     }
 }
--- Britt2022.A.A.ResultElements/InterfacesAbstractFactories/IResultElementsAbstractFactory.cs
+++ Britt2022.A.A.ResultElements/InterfacesAbstractFactories/IResultElementsAbstractFactory.cs
@@ -1,6 +1,13 @@
 ﻿namespace Britt2022.A.A.ResultElements.InterfacesAbstractFactories
 {
+    using Britt2022.A.A.ResultElements.InterfacesFactories.DayScenarioRecoveryWardCensuses;
+    using Britt2022.A.A.ResultElements.InterfacesFactories.ScenarioRecoveryWardCensuses;
+    using Britt2022.A.A.ResultElements.InterfacesFactories.SurgeonOperatingRoomDayAssignments;
+    using Britt2022.A.A.ResultElements.InterfacesFactories.SurgeonOperatingRoomDayScenarioDeviations;
+    using Britt2022.A.A.ResultElements.InterfacesFactories.SurgeonScenarioDeviations;
+
     public interface IResultElementsAbstractFactory
     {
+        IxResultElementFactory CreatexResultElementFactory();
     }
 }
