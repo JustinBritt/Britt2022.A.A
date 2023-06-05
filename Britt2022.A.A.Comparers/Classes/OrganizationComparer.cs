11d4f0ca6fa8a31759c81c249cbb0e194d870628
Justin Britt <britt@uwindsor.ca>
2023-06-05 17:32:34 -0400

    Added comparer

---
+++ Britt2022.A.A.Comparers/Classes/OrganizationComparer.cs
@@ -0,0 +1,24 @@
+﻿namespace Britt2022.A.A.Comparers.Classes
+{
+    using System;
+
+    using Hl7.Fhir.Model;
+
+    using Britt2022.A.A.Comparers.Interfaces;
+
+    internal sealed class OrganizationComparer : IOrganizationComparer
+    {
+        public OrganizationComparer()
+        {
+        }
+
+        public int Compare(
+            Organization x,
+            Organization y)
+        {
+            return String.CompareOrdinal(
+                x.Id,
+                y.Id);
+        }
+    }
+}
