247a678746a09ce8d9c801cd1aea064631de98b4
Justin Britt <britt@uwindsor.ca>
2023-06-05 21:01:50 -0400

    Added factory

---
+++ Britt2022.A.A.Comparers/Factories/OrganizationComparerFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace Britt2022.A.A.Comparers.Factories
+{
+    using Britt2022.A.A.Comparers.Classes;
+    using Britt2022.A.A.Comparers.Interfaces;
+    using Britt2022.A.A.Comparers.InterfacesFactories;
+
+    internal sealed class OrganizationComparerFactory : IOrganizationComparerFactory
+    {
+        public OrganizationComparerFactory()
+        {
+        }
+
+        public IOrganizationComparer Create()
+        {
+            IOrganizationComparer instance = null;
+
+            try
+            {
+                instance = new OrganizationComparer();
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+    }
+}
