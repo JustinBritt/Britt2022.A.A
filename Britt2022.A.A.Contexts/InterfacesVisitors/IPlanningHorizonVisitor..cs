fa3aa3e32d9a5892aa20a6ec1b40590d14d0242b
Justin Britt <britt@uwindsor.ca>
2023-06-21 10:57:27 -0400

    Added visitor

---
+++ Britt2022.A.A.Contexts/InterfacesVisitors/IPlanningHorizonVisitor..cs
@@ -0,0 +1,15 @@
+﻿namespace Britt2022.A.A.Contexts.InterfacesVisitors
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.Patterns.Visitor;
+    
+    public interface IPlanningHorizonVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : INullableValue<int>
+        where TValue : FhirDateTime
+    {
+        FhirDateTime[] GetValue();
+    }
+}
---
+++ Britt2022.A.A.Contexts/Visitors/PlanningHorizonVisitor..cs
@@ -0,0 +1,40 @@
+﻿namespace Britt2022.A.A.Contexts.Visitors
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using Britt2022.A.A.Contexts.InterfacesVisitors;
+    using Britt2022.A.A.IndexElements.InterfacesFactories;
+
+    internal sealed class PlanningHorizonVisitor<TKey, TValue> : IPlanningHorizonVisitor<TKey, TValue>
+        where TKey : INullableValue<int>
+        where TValue : FhirDateTime
+    {
+        public PlanningHorizonVisitor(
+            IkIndexElementFactory kIndexElementFactory)
+        {
+            this.kIndexElementFactory = kIndexElementFactory;
+
+            this.Value = new List<FhirDateTime>();
+        }
+
+        private IkIndexElementFactory kIndexElementFactory { get; }
+
+        public bool HasCompleted => false;
+
+        private List<FhirDateTime> Value { get; }
+
+        public FhirDateTime[] GetValue()
+        {
+            return this.Value.ToArray();
+        }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            this.Value.Add(
+                obj.Value);
+        }
+    }
+}
