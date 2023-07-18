9a41026735ea21b2392d317de41a3d5d54731211
Justin Britt <britt@uwindsor.ca>
2023-07-18 08:47:39 -0400

    Added factory

---
+++ Britt2022.A.A.Contexts/Factories/PlanningHorizonVisitorFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace Britt2022.A.A.Contexts.Factories
+{
+    using Hl7.Fhir.Model;
+
+    using Britt2022.A.A.Contexts.InterfacesFactories;
+    using Britt2022.A.A.Contexts.InterfacesVisitors;
+    using Britt2022.A.A.Contexts.Visitors;
+
+    internal sealed class PlanningHorizonVisitorFactory : IPlanningHorizonVisitorFactory
+    {
+        public PlanningHorizonVisitorFactory()
+        {
+        }
+
+        public IPlanningHorizonVisitor<TKey, TValue> Create<TKey, TValue>()
+            where TKey : INullableValue<int>
+            where TValue : FhirDateTime
+        {
+            IPlanningHorizonVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new PlanningHorizonVisitor<TKey, TValue>();
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.A.Contexts/InterfacesFactories/IPlanningHorizonVisitorFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Britt2022.A.A.Contexts.InterfacesFactories
+{
+    using Hl7.Fhir.Model;
+
+    using Britt2022.A.A.Contexts.InterfacesVisitors;
+
+    public interface IPlanningHorizonVisitorFactory
+    {
+        IPlanningHorizonVisitor<TKey, TValue> Create<TKey, TValue>()
+            where TKey : INullableValue<int>
+            where TValue : FhirDateTime;
+    }
+}
