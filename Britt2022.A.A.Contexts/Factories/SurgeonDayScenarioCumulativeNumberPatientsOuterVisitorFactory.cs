12b77813a10167247743232a406a91e4f047157e
Justin Britt <britt@uwindsor.ca>
2023-07-20 02:21:05 -0400

    Added factory

---
+++ Britt2022.A.A.Contexts/Factories/SurgeonDayScenarioCumulativeNumberPatientsOuterVisitorFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace Britt2022.A.A.Contexts.Factories
+{
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Britt2022.A.A.Contexts.InterfacesFactories;
+    using Britt2022.A.A.Contexts.InterfacesVisitors;
+    using Britt2022.A.A.Contexts.Visitors;
+    using Britt2022.A.A.ParameterElements.InterfacesFactories.LengthsOfStay;
+
+    internal sealed class SurgeonDayScenarioCumulativeNumberPatientsOuterVisitorFactory : ISurgeonDayScenarioCumulativeNumberPatientsOuterVisitorFactory
+    {
+        public SurgeonDayScenarioCumulativeNumberPatientsOuterVisitorFactory()
+        {
+        }
+
+        public ISurgeonDayScenarioCumulativeNumberPatientsOuterVisitor<TKey, TValue> Create<TKey, TValue>(
+            IΦParameterElementFactory ΦParameterElementFactory)
+            where TKey : Organization
+            where TValue : RedBlackTree<INullableValue<int>, RedBlackTree<INullableValue<int>, INullableValue<decimal>>>
+        {
+            ISurgeonDayScenarioCumulativeNumberPatientsOuterVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new SurgeonDayScenarioCumulativeNumberPatientsOuterVisitor<TKey, TValue>(
+                    ΦParameterElementFactory);
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Britt2022.A.A.Contexts/InterfacesFactories/ISurgeonDayScenarioCumulativeNumberPatientsOuterVisitorFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace Britt2022.A.A.Contexts.InterfacesFactories
+{
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Britt2022.A.A.Contexts.InterfacesVisitors;
+    using Britt2022.A.A.ParameterElements.InterfacesFactories.LengthsOfStay;
+
+    public interface ISurgeonDayScenarioCumulativeNumberPatientsOuterVisitorFactory
+    {
+        ISurgeonDayScenarioCumulativeNumberPatientsOuterVisitor<TKey, TValue> Create<TKey, TValue>(
+            IΦParameterElementFactory ΦParameterElementFactory)
+            where TKey : Organization
+            where TValue : RedBlackTree<INullableValue<int>, RedBlackTree<INullableValue<int>, INullableValue<decimal>>>;
+    }
+}
