b6a93785879e8ed39c42037611990e9feaf898ee
Justin Britt <britt@uwindsor.ca>
2023-07-21 02:23:24 -0400

    Added class

---
+++ Britt2022.A.A.Comparers/Classes/LocationComparer.cs
@@ -0,0 +1,24 @@
+﻿namespace Britt2022.A.A.Comparers.Classes
+{
+    using System;
+
+    using Hl7.Fhir.Model;
+
+    using Britt2022.A.A.Comparers.Interfaces;
+
+    internal sealed class LocationComparer : ILocationComparer
+    {
+        public LocationComparer()
+        {
+        }
+
+        public int Compare(
+            Location x,
+            Location y)
+        {
+            return String.CompareOrdinal(
+                x.Id,
+                y.Id);
+        }
+    }
+}
---
+++ Britt2022.A.A.Comparers/Interfaces/ILocationComparer.cs
@@ -0,0 +1,10 @@
+﻿namespace Britt2022.A.A.Comparers.Interfaces
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    public interface ILocationComparer : IComparer<Location>
+    {
+    }
+}
