a883369bed4aed8bc386d16b08c2a815a49be6a7
Justin Britt <britt@uwindsor.ca>
2023-07-21 01:48:09 -0400

    Added class

---
+++ Britt2022.A.A.Comparers/Classes/NullableValueintComparer.cs
@@ -0,0 +1,21 @@
+﻿namespace Britt2022.A.A.Comparers.Classes
+{
+    using Hl7.Fhir.Model;
+
+    using Britt2022.A.A.Comparers.Interfaces;
+
+    internal sealed class NullableValueintComparer : INullableValueintComparer
+    {
+        public NullableValueintComparer()
+        {
+        }
+
+        public int Compare(
+            INullableValue<int> x,
+            INullableValue<int> y)
+        {
+            return x.Value.Value.CompareTo(
+                y.Value.Value);
+        }
+    }
+}
