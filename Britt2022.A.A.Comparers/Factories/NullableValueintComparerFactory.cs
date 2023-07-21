0aa2b7c2ed4c5d66591752025f07e57c384dd75a
Justin Britt <britt@uwindsor.ca>
2023-07-21 01:53:10 -0400

    Added factory

---
+++ Britt2022.A.A.Comparers/Factories/NullableValueintComparerFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace Britt2022.A.A.Comparers.Factories
+{
+    using Britt2022.A.A.Comparers.Classes;
+    using Britt2022.A.A.Comparers.Interfaces;
+    using Britt2022.A.A.Comparers.InterfacesFactories;
+
+    internal sealed class NullableValueintComparerFactory : INullableValueintComparerFactory
+    {
+        public NullableValueintComparerFactory()
+        {
+        }
+
+        public INullableValueintComparer Create()
+        {
+            INullableValueintComparer instance = null;
+
+            try
+            {
+                instance = new NullableValueintComparer();
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+    }
+}
