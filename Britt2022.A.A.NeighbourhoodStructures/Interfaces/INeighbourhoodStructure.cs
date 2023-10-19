a844f239b8a8487d6ed379c30a74202a3a082d79
Justin Britt <britt@uwindsor.ca>
2023-10-19 15:04:35 -0400

    Changes to usings; Implement interface

--- Britt2022.A.A.NeighbourhoodStructures/Interfaces/INeighbourhoodStructure.cs
+++ Britt2022.A.A.NeighbourhoodStructures/Interfaces/INeighbourhoodStructure.cs
@@ -2,11 +2,12 @@
 {
     using System;
 
+    using Britt.A.A.LLH.Interfaces;
     using Britt2022.A.A.IndexElements.Structs;
     using Britt2022.A.A.ParameterElements.Structs.SurgicalSpecialties;
     using Britt2022.A.A.VariableElements.Structs;
 
-    public interface INeighbourhoodStructure
+    public interface INeighbourhoodStructure : ILowLevelHeuristic
     {
         bool SwapMade { get; set; }
 
